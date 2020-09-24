59c4bdf976f6959f48af260d989283ba56c3a032
Emmanuel Durgoni <emmanuel@upian.com>
2020-09-24 11:21:58 +0200

    update css

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const h1 = document.querySelector('h1')
+console.log(h1)
+
+h1.classList.toggle('my-title')
+
+const second = 3
+window.setTimeout(() => {
+  h1.classList.toggle('my-title')
+}, second * 1000)
+
+const allParagraphs = document.querySelectorAll('p')
+console.log(allParagraphs)
+
+// Ajouter une classe aux element qui ont un index pair
+for(let i = 0; i < allParagraphs.length; i++) {
+  const el = allParagraphs[i]
+  console.log(el)
+  // On teste si l'index est pair
+  if (i % 2 === 0) {
+    el.classList.add('even-paragraph')
+  }
+}
