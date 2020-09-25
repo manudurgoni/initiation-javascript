ef7a99de8c6331cd841ac42d508aa2b4e1654650
Emmanuel Durgoni <emmanuel@upian.com>
2020-09-25 16:29:19 +0200

    up

--- vue-weather/src/components/Form.vue
+++ vue-weather/src/components/Form.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <form class="Form" @submit.prevent="getWeatherByCity">
-      <input type="text" placeholder="Selectionnez une ville" v-model="city">
+      <input type="text" placeholder="Selectionnez une ville" v-model="city" @input="onChange">
       <input type="submit" value="Ok">
     </form>
+
+    <div class="error" v-if="error">
+      <h2>{{ city }} n'existe pas !</h2>
+    </div>
 
     <v-result v-if="result" :city="result"></v-result>
   </div>
@@ -17,17 +21,28 @@ export default {
   },
   data() {
     return {
+      error: false,
       city: null,
       result: null
     }
   },
 
   methods: {
+    onChange() {
+      this.error = false
+    },
     async getWeatherByCity() {
+      this.error = false
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=31e6e989590a80e12446183d2fc1d332&units=metric`
 
       let response = await fetch(url)
       let json = await response.json()
+      
+      if (json.cod === '404') {
+        this.result = null
+        this.error = true
+        return
+      }
 
       this.result = json
     }
