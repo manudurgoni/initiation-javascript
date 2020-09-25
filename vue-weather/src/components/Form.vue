08987730901b9f371e440ef02fb36d02426e2b55
Emmanuel Durgoni <emmanuel@upian.com>
2020-09-25 15:32:15 +0200

    up

--- vue-weather/src/components/Form.vue
+++ vue-weather/src/components/Form.vue
@@ -1,12 +1,20 @@
 <template>
-  <form class="Form" @submit.prevent="getWeatherByCity">
-    <input type="text" placeholder="Selectionnez une ville" v-model="city">
-    <input type="submit" value="Ok">
-  </form>
+  <div>
+    <form class="Form" @submit.prevent="getWeatherByCity">
+      <input type="text" placeholder="Selectionnez une ville" v-model="city">
+      <input type="submit" value="Ok">
+    </form>
+
+    <v-result v-if="result"></v-result>
+  </div>
 </template>
 
 <script>
+import VResult from './Result'
 export default {
+  components: {
+    VResult
+  },
   data() {
     return {
       city: null,
@@ -16,13 +24,12 @@ export default {
 
   methods: {
     async getWeatherByCity() {
-      console.log(this.city)
-
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=31e6e989590a80e12446183d2fc1d332&units=metric`
 
       let response = await fetch(url)
       let json = await response.json()
-      console.log(json)
+
+      this.result = json
     }
   }
 }
---
+++ vue-weather/src/components/Result.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Ville</h2>
+    <div class="temperature">
+      <p class="temperature__current">temp current</p>
+      <p class="temperature__min">temp min</p>
+      <p class="temperature__max">temp max</p>
+    </div>
+  </div>
+</template>
+
