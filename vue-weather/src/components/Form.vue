2417d236980d0b762c0406af21e35dad45ae1587
Emmanuel Durgoni <emmanuel@upian.com>
2020-09-25 15:16:48 +0200

    up

--- vue-weather/src/components/Form.vue
+++ vue-weather/src/components/Form.vue
@@ -1,6 +1,25 @@
 <template>
   <form class="Form">
-    <input type="text" placeholder="Selectionnez une ville">
-    <input type="submit" value="">
+    <input type="text" placeholder="Selectionnez une ville" v-model="city">
+    <input type="submit" value="Ok">
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      city: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  display: block;
+  margin: 10px 0;
+  width: 100%;
+  padding: 10px;
+}
+</style>
